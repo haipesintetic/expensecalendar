c54cc499b349ba8a0dba3acf5a7cadd6eb65cc20
haipesintetic <sorinturculet@gmail.com>
2023-09-15 16:15:49 +0300

    Added my project

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ChartBar from "/ChartBar.js";
+import "./Chart.css";
+function Chart(props) {
+  return (
+    <div className="chart">
+      {props.dataPoints.map((dataPoint) => (
+        <ChartBar
+            key={dataPoint.label}
+          value={dataPoint.value}
+          maxValue={null}
+          label={dataPoint.label}
+        />
+      ))}
+    </div>
+  );
+}
+export default Chart;
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,17 +1,27 @@
-import React from "react";
+import React,{useState} from "react";
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 function NewExpense(props) {
+  const[isEditing,SetIsEditing]=useState(false);
+
   function saveExpenseDataHandler(enteredExpenseData) {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    SetIsEditing(false);
+  }
+  function startEditingHandler(){
+    SetIsEditing(true);
+  }
+  function stopEditingHandler(){
+    SetIsEditing(false);
   }
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!isEditing && <button onClick={startEditingHandler}> Add New Expense</button>}
+      {isEditing && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={stopEditingHandler}/> }
     </div>
   );
 }
